707ecd5e2746a41f603643c23321cc3b68abf054
Ziyi Yan <ziyi.yan@foxmail.com>
2020-08-25 18:10:05 +0800

    improve 930.binary-subarrays-with-sum.cpp

--- 930.binary-subarrays-with-sum.cpp
+++ 930.binary-subarrays-with-sum.cpp
@@ -13,14 +13,23 @@ class Solution
 public:
     int numSubarraysWithSum(vector<int> &A, int S)
     {
-        auto count = 0;
-        unordered_map<int, int> prefix_sum_count = {{0, 1}};
-        auto curr = 0;
-        for (auto i : A)
+        // NOTE: Convert the equality into a minus of two at-most result.
+        return atMost(A, S) - atMost(A, S - 1);
+    }
+
+private:
+    int atMost(vector<int> &A, int S)
+    {
+        if (S < 0)
+            return 0;
+        int prefix_sum = 0;
+        int count = 0;
+        for (auto i = 0, j = 0; j < A.size();)
         {
-            curr += i;
-            count += prefix_sum_count[curr - S];
-            prefix_sum_count[curr]++;
+            prefix_sum += A[j++];
+            while (prefix_sum > S)
+                prefix_sum -= A[i++];
+            count += j - i;
         }
         return count;
     }
