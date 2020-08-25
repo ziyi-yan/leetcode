f308f930136acc6aa853867efc226c7c5d0c4d86
Ziyi Yan <ziyi.yan@foxmail.com>
2020-08-25 16:21:41 +0800

    improve 1470.shuffle-the-array.cpp

--- 1470.shuffle-the-array.cpp
+++ 1470.shuffle-the-array.cpp
@@ -12,13 +12,23 @@ class Solution
 public:
     vector<int> shuffle(vector<int> &nums, int n)
     {
-        vector<int> result;
+        for (auto i = n - 1, j = 2 * n - 1; i >= 0; i--, j--)
+        {
+            nums[j] = (nums[i] << 10) | nums[j];
+        }
         for (auto i = 0; i < n; i++)
         {
-            result.push_back(nums[i]);
-            result.push_back(nums[n + i]);
+            nums[2 * i] = nums[n + i] >> 10;
+            nums[2 * i + 1] = nums[n + i] & ((1 << 10) - 1);
         }
-        return result;
+
+        return nums;
     }
 };
 // @lc code=end
+int main()
+{
+    vector<int> input = {2, 5, 1, 3, 4, 7};
+    int n = 3;
+    auto result = Solution().shuffle(input, n);
+}
