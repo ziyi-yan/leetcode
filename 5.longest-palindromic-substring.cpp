087a4900cf5225019beb8b84ad8dd4a655913ff9
Ziyi Yan <ziyi.yan@foxmail.com>
2020-09-15 00:06:58 +0800

    improve manacher's algorithm 5.longest-palindromic-substring.cpp

--- 5.longest-palindromic-substring.cpp
+++ 5.longest-palindromic-substring.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 using namespace std;
 /*
  * @lc app=leetcode id=5 lang=cpp
@@ -7,45 +8,52 @@ using namespace std;
  */
 
 // @lc code=start
-class Solution
-{
-public:
-    string longestPalindrome(string s)
-    {
-        if (s.empty())
-            return "";
+class Solution {
+   public:
+    string longestPalindrome(string s) {
+        int largest_l = 0;
+        int largest_r = 0;
 
-        int largest = 0;
-        string result;
-        int l = 0, r = 0;
-        while (r < s.size())
-        {
-            int i = l, j = r;
-            while (i >= 0 && j < s.size() && s[i] == s[j])
-            {
-                auto len = j - i + 1;
-                if (len > largest)
-                {
-                    largest = len;
-                    result = s.substr(i, largest);
-                }
-
-                i--;
-                j++;
+        int n = s.size();
+        vector<int> d1(n);
+        for (int i = 0, l = 0, r = -1; i < n; i++) {
+            // k is the minimal possible expansion length centered at i
+            int k = i > r ? 1 : min(r - i + 1, d1[l + (r - i)]);
+            while (i + k < n && i - k >= 0 && s[i + k] == s[i - k]) {
+                k++;
             }
-
-            if (l == r)
-                r++;
-            else
-                l++;
+            d1[i] = k;
+            if (i + k - 1 > r) {
+                r = i + k - 1;
+                l = i - k + 1;
+            }
+            if (r - l > largest_r - largest_l) {
+                largest_l = l;
+                largest_r = r;
+            }
         }
-
-        return result;
+        vector<int> d2(n);
+        for (int i = 0, l = 0, r = -1; i < n; i++) {
+            int k = i > r ? 0 : min(r - i + 1, d2[l + r - i + 1]);
+            while (0 <= i - k - 1 && i + k < n && s[i - k - 1] == s[i + k]) {
+                k++;
+            }
+            d2[i] = k--;
+            if (i + k - 1 > r) {
+                l = i - k - 1;
+                r = i + k;
+            }
+            if (r - l > largest_r - largest_l) {
+                largest_l = l;
+                largest_r = r;
+            }
+        }
+        return s.substr(largest_l, largest_r - largest_l + 1);
     }
 };
 // @lc code=end
-int main()
-{
-    // auto result = Solution().longestPalindrome("aaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaa");
+int main() {
+    // auto result =
+    // Solution().longestPalindrome("aaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaa");
     auto result = Solution().longestPalindrome("babad");
 }
